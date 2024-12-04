e3af4301fb466a7bc79330427b888f28af07da85
benedek.bence <benedek.bence@ckik.hu>
2024-12-04 11:12:53 +0100

    parosok megvannak

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -9,9 +9,33 @@ namespace Progtetel
             int[] x = { 7, 2, -4, 5, 12, 1 };
 
             //Párosak kiválogatása - 1
-
+            int[] parosok = new int[100];
+            int db = 0;
+            int i;
+            for ( i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    parosok[db] = x[i];
+                    db++;
+                }
+            }
+            for ( i = 0; i < db; i++)
+            {
+                Console.Write(parosok[i] + " ");
+            }
             //Maximum kiválasztás - 2
-            
+            //int n = x.Length;
+            //int maxe = x[0];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (x[i] > maxe)
+            //    {
+            //        maxe = x[i];
+            //    }
+            //}
+            //Console.WriteLine(maxe);
+            //Console.ReadKey();
         }
     }
 }
