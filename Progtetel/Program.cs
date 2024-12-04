61848117484c63f25c8e428f620508be980da372
magda.peter <magda.peter@ckik.hu>
2024-12-04 11:02:31 +0100

    valtoztatasok

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-
 namespace Progtetel
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int[] x = { 7, 2, -4, 5, 12, 1 };
+
+            //Párosak kiválogatása - 1
+
+            //Maximum kiválasztás - 2
+            
         }
     }
 }
